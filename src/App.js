54e32fe1b78a5579a4984c301b36f4e0f4afabed
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2023-11-08 23:08:56 +0530

    new-changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import SearchBox from "./components/SearchBox";
 import Button from "./components/Button";
+import HeroComponent from "./components/HeroComponent";
+import CardComponent from "./components/Card";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <HeroComponent />
+      <Router>
+        <Routes>
+          <Route path="/album/:slug" element={<CardComponent />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
