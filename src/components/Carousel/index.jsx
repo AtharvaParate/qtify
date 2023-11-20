ea760da9d0550577123c4f6443b791c9d6bbad92
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2023-11-20 23:02:36 +0530

    carousel

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,49 @@
+import { Virtual, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./carousel.css";
+import Card from "../Card";
+
+import LeftArrow from "./leftArrow.png";
+import RightArrow from "./rightArrow.png";
+
+export default ({ data }) => {
+  return (
+    <div className="carousel-container">
+      <Swiper
+        modules={[Virtual, Navigation, Pagination]}
+        // onSwiper={setSwiperRef}
+        slidesPerView={8}
+        // centeredSlides={true}
+        spaceBetween={30}
+        // pagination={{
+        //   type: "fraction",
+        // }}
+        // navigation={true}
+        navigation={{ nextEl: ".arrow-right", prevEl: ".arrow-left" }}
+        virtual
+      >
+        {data.map((cardData) => (
+          <SwiperSlide key={cardData.id}>
+            <Card
+              imgSrc={cardData.image}
+              label={cardData.title}
+              followersCount={cardData.follows}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="arrow-left arrow">
+        <img src={LeftArrow}></img>
+      </div>
+      <div className="arrow-right arrow">
+        <img src={RightArrow}></img>
+      </div>
+    </div>
+  );
+};
