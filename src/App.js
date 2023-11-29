e70ab4f0646884e2585808efcce6999afa015ffa
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2023-11-29 16:37:14 +0530

    filteredSection

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import HeroComponent from "./components/HeroComponent";
 import Section from "./components/Section";
+import FilterSection from "./components/FilterSection";
 
 const ENDPOINT = "https://qtify-backend-labs.crio.do/";
 
 function App() {
   const [topAlbums, setTopAlbums] = useState([]);
   const [newAlbums, setNewAlbums] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [filteredSongs, setFilteredSongs] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     axios.get(`${ENDPOINT}albums/top`).then(({ data }) => {
       setTopAlbums(data);
     });
-    axios.get(`${ENDPOINT}albums/new`).then(({ data }) => {
-      setNewAlbums(data);
+    axios.get(`${ENDPOINT}albums/new`).then((res) => {
+      setNewAlbums(res.data);
+    });
+    axios.get(`${ENDPOINT}songs`).then((res) => {
+      setSongs(res.data);
+      setFilteredSongs(res.data);
+    });
+    axios.get(`${ENDPOINT}genres`).then((res) => {
+      setGenres([{ key: "all", label: "All" }, ...res.data.data]);
     });
   }, []);
 
@@ -26,6 +37,18 @@ function App() {
       <HeroComponent />
       <Section title="Top Albums" data={topAlbums} />
       <Section title="New Albums" data={newAlbums} />
+      <FilterSection
+        title="Songs"
+        data={filteredSongs}
+        filters={genres}
+        executeFilter={(genre) => {
+          if (genre === "all") {
+            setFilteredSongs(songs);
+          } else {
+            setFilteredSongs(songs.filter((song) => song.genre.key === genre));
+          }
+        }}
+      />
     </>
   );
 }
