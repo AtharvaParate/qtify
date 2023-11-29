ea5b2a8104aa3e5343d193620bc6b51f6c4a5b51
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2023-11-29 23:28:07 +0530

    search-without-api

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,17 +1,24 @@
 import { ReactComponent as SearchIcon } from "./search-icon.svg";
 import "./searchBox.css";
-
+import Menu from "../Menu";
 
 export default () => {
   return (
-    <form style={{ display: "flex", minWidth: "568px" }}>
-      <input
-        placeholder="Search an album of your choice"
-        style={{ flex: "1", borderRadius: "8px 0px 0px 8px", padding: "8px" }}
-      />
-      <button className="search">
-        <SearchIcon width={20} height={20} />
-      </button>
-    </form>
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      <form style={{ display: "flex", minWidth: "570px" }}>
+        <input
+          placeholder="Search an album of your choice"
+          style={{ flex: "1", borderRadius: "8px 0px 0px 8px", padding: "8px" }}
+        />
+        <button className="search">
+          <SearchIcon width={20} height={20} />
+        </button>
+      </form>
+      <Menu data={[1, 2, 3, 4, 5, 6]} />
+    </div>
   );
 };
